ca7a2352bcd9ead72cbad16b82a3fcbda5ca5712
bsivanantham <balavivek107@gmail.com>
2018-07-21 15:01:00 +0200

    Fxml ui commit

--- src/main/java/de/unibi/citec/clf/App.java
+++ src/main/java/de/unibi/citec/clf/App.java
@@ -6,8 +6,25 @@ package de.unibi.citec.clf;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        BorderPane root = new BorderPane();
+        
+        Scene scene = new Scene(root,640,480);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ui.fxml"));
+        scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
+
+        loader.setControllerFactory(t -> new EditorController(new EditorModel()));
+        
+        stage.setScene(new Scene(loader.load()));
+        
+        stage.show();
+        
+        root.setCenter(new RootLayout());
+    }
+    
+    public static void main(String[] args){
+        launch(args);
     }
 }
